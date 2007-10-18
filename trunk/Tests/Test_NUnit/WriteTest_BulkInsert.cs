b6a12220993f63beb984c12cb941ebf966d2c5e8
gmoudry <gmoudry@33146b6e-df29-0410-a1ca-39be37fb9416>
2007-10-18 07:23:57 +0000

    added doc

---
+++ trunk/Tests/Test_NUnit/WriteTest_BulkInsert.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Linq.Expressions;
+using NUnit.Framework;
+using Client2.user;
+
+namespace Test_NUnit
+{
+    [TestFixture]
+    class WriteTest_BulkInsert : TestBase
+    {
+    }
+}
