6ffa2dd2bc87b6b57e16b5cb893a2a3c3436f3e5
gmoudry <gmoudry@33146b6e-df29-0410-a1ca-39be37fb9416>
2007-10-28 12:19:56 +0000

    Re-licensed from LGPL to MIT license to be compatible with Mono project.

--- trunk/SqlMetal/schema/pgsql/PgsqlVendor.cs
+++ trunk/SqlMetal/schema/pgsql/PgsqlVendor.cs
@@ -1,8 +1,25 @@
 ////////////////////////////////////////////////////////////////////
-//Initial author: Jiri George Moudry, 2006.
-//License: LGPL. (Visit http://www.gnu.org)
-//Commercial code may call into this library, if it's in a different module (DLL).
-//All changes to this library must be published.
+// MIT license:
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+//
+// Authors:
+//        Jiri George Moudry
 ////////////////////////////////////////////////////////////////////
 
 //Sources: Extracting META Information from PostgreSQL - Lorenzo Alberton
