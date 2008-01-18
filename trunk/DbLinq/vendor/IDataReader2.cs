37955fcfa86f4120d880657846949635d6d65af7
gmoudry <gmoudry@33146b6e-df29-0410-a1ca-39be37fb9416>
2008-01-18 20:51:49 +0000

    Added comments and missing methods

--- trunk/DbLinq/vendor/IDataReader2.cs
+++ trunk/DbLinq/vendor/IDataReader2.cs
@@ -5,7 +5,17 @@ using System.Text;
 
 namespace DBLinq.vendor
 {
-    public interface IDataReader2
+    /// <summary>
+    /// This interface lists all the functions required on top of regular IDataReader.
+    /// 
+    /// IDataReader2 is something that is no longer necessary.
+    /// In 2006 version of Linq, the compiled expression 'reader.IsDbNull(0)?null:reader.GetInt32(0)' would crash the VM.
+    /// Instead, we had to call into hand-written functions - such as GetInt32N().
+    /// I haven't checked, but I assume that bug is fixed - 
+    /// but I found it very useful to be able to set breakpoints in  the reader function.
+    /// </summary>
+    public interface IDataReader2 
+        //: System.Data.IDataReader //causes too many errors: missing NextResult(), Depth, ...
     {
         short? GetInt16N(int index);
         char? GetCharN(int index);
@@ -16,10 +26,29 @@ namespace DBLinq.vendor
         float? GetFloatN(int index);
         DateTime? GetDateTimeN(int index);
         long? GetInt64N(int index);
-        //why not a GetByte()?
+
         /// <summary>
         /// method to read a blob
         /// </summary>
         byte[] GetBytes(int index);
+
+        #region regular IDataReader functions that we used in compiled code
+
+        bool Read();
+        int FieldCount { get; }
+
+        bool GetBoolean(int index);
+        byte GetByte(int index);
+        char GetChar(int index);
+        DateTime GetDateTime(int index);
+        decimal GetDecimal(int index);
+        double GetDouble(int index);
+        float GetFloat(int index);
+        short GetInt16(int index);
+        int GetInt32(int index);
+        long GetInt64(int index);
+        uint GetUInt32(int index);
+
+        #endregion
     }
 }
