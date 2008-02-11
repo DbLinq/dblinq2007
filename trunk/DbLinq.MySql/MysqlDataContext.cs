c8b881c217f75fc98a08a62d552c04fe60c7b5d9
gmoudry <gmoudry@33146b6e-df29-0410-a1ca-39be37fb9416>
2008-02-11 22:54:17 +0000

    Preparing changes to SqlMetal to fix generation. Preparing changes to IVendor to support connection opening/closing, and params in ExecuteCommand.

---
+++ trunk/DbLinq.MySql/MysqlDataContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+namespace DBLinq.vendor.mysql
+{
+    public class MysqlDataContext : DBLinq.Linq.DataContext
+    {
+        public MysqlDataContext(string connStr)
+            : base(new MySqlConnection(connStr), new VendorMysql())
+        {
+        }
+
+        public MysqlDataContext(IDbConnection conn)
+            : base((MySqlConnection)conn, new VendorMysql())
+        {
+        }
+
+    }
+}
---
+++ trunk/Tests/Test_NUnit/CompositePK_Test.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using nwind;
+
+namespace Test_NUnit
+{
+    public class CompositePK_Test : TestBase
+    {
+    }
+}
