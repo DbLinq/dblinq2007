a03bb84d69792b83f4dd2822eb88729d44f890ea
Pascal Craponne <pascal@craponne.fr>
2008-02-11 21:37:18 +0000

    Added IDataMapper interface (instanciated to DataMapper by default) which allows replace the default data mapper

---
+++ DbLinq/Linq/IDataMapper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DBLinq.vendor;
+
+namespace DBLinq.Linq
+{
+    public interface IDataMapper
+    {
+        Func<IDataReader2, T> GetMapper<T>(SessionVarsParsed vars);
+    }
+}
