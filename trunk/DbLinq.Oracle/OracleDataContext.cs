b09dadfca91f26c72f1eeb66188908af3bbcaec2
gmoudry <gmoudry@33146b6e-df29-0410-a1ca-39be37fb9416>
2008-02-12 20:28:55 +0000

    Adding [Mysql]DataContext for each Provider

---
+++ trunk/DbLinq.Oracle/OracleDataContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+
+#if ORACLE_IS_REFERENCED
+//seems that Pascal does not reference Oracle DLLs from here?
+using Oracle.Data.OracleClient;
+#endif
+
+namespace DbLinq.Oracle
+{
+    public class OracleDataContext : DBLinq.Linq.DataContext
+    {
+#if ORACLE_IS_REFERENCED
+        public OracleDataContext(string connStr)
+            : base(
+#if ODP
+            new Oracle.DataAccess.Client.OracleConnection(connStr) 
+#else
+            new System.Data.OracleClient.OracleConnection(connStr)
+#endif
+            ,new OracleVendor())
+        {
+        }
+#endif
+
+        public OracleDataContext(IDbConnection conn)
+            : base(conn, new OracleVendor())
+        {
+        }
+
+    }
+}
---
+++ trunk/DbLinq.Sqlite/SqliteDataContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SQLite;
+
+namespace DbLinq.Sqlite
+{
+    public class SqliteDataContext : DBLinq.Linq.DataContext
+    {
+        public SqliteDataContext(string connStr)
+            : base(new SQLiteConnection(connStr), new SqliteVendor())
+        {
+        }
+
+        public SqliteDataContext(IDbConnection conn)
+            : base((SQLiteConnection)conn, new SqliteVendor())
+        {
+        }
+
+    }
+}
