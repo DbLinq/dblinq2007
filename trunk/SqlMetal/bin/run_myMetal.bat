601278ea54e9826761dc05c72c81e30815fd1695
gmoudry <gmoudry@33146b6e-df29-0410-a1ca-39be37fb9416>
2008-02-18 23:04:38 +0000

    SqlMetal changes after refactoring

--- trunk/SqlMetal/bin/run_myMetal.bat
+++ trunk/SqlMetal/bin/run_myMetal.bat
@@ -1,6 +1,5 @@
 REM: note that the '-sprocs' option is turned on
-REM MySqlMetal.exe -database:Northwind -server:localhost -user:LinqUser -password:linq2 -namespace:nwind -dbml:nwind_mysql.dbml -sprocs
 
-MySqlMetal.exe -database:Northwind -server:localhost -user:LinqUser -password:linq2 -namespace:nwind -code:Northwind.cs -sprocs
+SqlMetal.exe -provider=MySql -database:Northwind -server:localhost -user:LinqUser -password:linq2 -namespace:nwind -code:Northwind.cs -sprocs
 
 
