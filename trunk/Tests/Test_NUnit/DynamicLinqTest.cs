ae25802d0c853cf9ba20ebf6fc0a9a5bdd1346ed
gmoudry <gmoudry@33146b6e-df29-0410-a1ca-39be37fb9416>
2008-03-04 21:16:39 +0000

    Fix to allow Dynamic SQL. adding also lib and test DL1_Products()

---
+++ trunk/Tests/Test_NUnit/DynamicLinqTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using nwind;
+using System.Linq.Dynamic;
+
+namespace Test_NUnit
+{
+    [TestFixture]
+    public class DynamicLinqTest : TestBase
+    {
+        [Test]
+        public void DL1_Products()
+        {
+            Northwind db = CreateDB();
+
+            var q = db.Products.Where("SupplierID=1 And UnitsInStock>2")
+                .OrderBy("ProductID");
+            var list = q.ToList();
+            Assert.IsTrue(list.Count > 0, "Expected results from dynamic query");
+        }
+
+#if DECIDE_IF_THIS_CAST_IS_LEGAL
+        [Test]
+        public void DL2_Cast()
+        {
+            Northwind db = CreateDB();
+            
+            IQueryable<Customer> q =
+            db.Customers.Select("new(CustomerID,City)").Cast<Customer>();
+
+            var list = q.ToList();
+            Assert.IsTrue(list.Count > 0, "Expected results from dynamic query");
+        }
+#endif
+    }
+}
