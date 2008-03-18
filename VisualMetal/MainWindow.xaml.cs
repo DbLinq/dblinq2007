593deb4ecdb3be984cd44f820168f7e67eaebcbc
bryanlivingston <bryanlivingston@33146b6e-df29-0410-a1ca-39be37fb9416>
2008-03-18 22:47:29 +0000

    Visual Metal: Built Main Window Added displaying of tables and columns Added savings of Parameters Added optional saving of passwords

--- VisualMetal/MainWindow.xaml.cs
+++ VisualMetal/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Windows;
@@ -7,28 +8,79 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Markup;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using DbLinq.Vendor;
+using DbLinq.Schema.Dbml;
+using SqlMetal;
+
 namespace VisualMetal
 {
-	/// <summary>
-	/// Interaction logic for Window1.xaml
-	/// </summary>
 	public partial class MainWindow : Window
 	{
+		public SqlMetalParameters Parameters = new SqlMetalParameters();
+		public ISchemaLoader Loader;
+		public Database Database;
+
 		public MainWindow()
 		{
 			InitializeComponent();
+
+			if (!String.IsNullOrEmpty(Properties.Settings.Default.Params))
+				using (var stream = new MemoryStream(Encoding.UTF8.GetBytes(Properties.Settings.Default.Params)))
+					Parameters = (SqlMetalParameters)XamlReader.Load(stream);
 		}
 
-		protected override void OnActivated(EventArgs e)
+		protected override void OnClosed(EventArgs e)
 		{
-			base.OnActivated(e);
+			base.OnClosed(e);
 
-			new LoginWindow().ShowDialog();
+			if (!Properties.Settings.Default.SavePassword)
+				Parameters.Password = null; // clear password for security.
+			Properties.Settings.Default.Params = XamlWriter.Save(Parameters);
+			Properties.Settings.Default.Save();
+		}
+
+		private void Login_Click(object sender, RoutedEventArgs e)
+		{
+			new LoginWindow(this).ShowDialog();
+		}
+
+		public bool LoadSchema()
+		{
+			try
+			{
+				Loader = new LoaderFactory().Load(Parameters);
+				Database = SqlMetalProgram.LoadSchema(Parameters, Loader);
+
+				TableList.ItemsSource = Database.Table;
+			}
+			catch (Exception exception)
+			{
+				MessageBox.Show(exception.ToString());
+				return false;
+			}
+			return true;
+		}
+
+		private void Exit_Click(object sender, RoutedEventArgs e)
+		{
+			Close();
+		}
+
+		private void TableList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			if (e.AddedItems.Count == 0)
+				ColumnList.ItemsSource = null;
+			else
+			{
+				DbLinq.Schema.Dbml.Table selected = (DbLinq.Schema.Dbml.Table)e.AddedItems[0];
+				ColumnList.ItemsSource = selected.Type.Items;
+			}
 		}
 	}
 }
