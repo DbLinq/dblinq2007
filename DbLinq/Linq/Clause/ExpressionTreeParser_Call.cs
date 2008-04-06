2677b0b15a9a4de023f65fea81833ba5aafb7d35
Jiri Moudry <gmoudry@gmail.com>
2008-04-06 15:47:44 +0000

    refactoring of ExpressionTreeParser.AnalyzeMethodCall - break it into two files and 3 functions

---
+++ DbLinq/Linq/Clause/ExpressionTreeParser_Call.cs
@@ -0,0 +1,272 @@
+﻿#region MIT license
+////////////////////////////////////////////////////////////////////
+// MIT license:
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+//
+// Authors:
+//        Jiri George Moudry
+////////////////////////////////////////////////////////////////////
+#endregion
+
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq.Expressions;
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
+using DbLinq.Util;
+using DbLinq.Util.ExprVisitor;
+using DbLinq.Linq.Mapping;
+using DbLinq.Vendor;
+namespace DbLinq.Linq.Clause
+{
+    public partial class ExpressionTreeParser
+    {
+        internal void AnalyzeMethodCall(RecurData recurData, MethodCallExpression expr)
+        {
+            string methodName = expr.Method.Name;
+
+            string sqlOperatorName;
+            if (s_csharpOperatorToSqlMap.TryGetValue(methodName, out sqlOperatorName))
+            {
+                //map "op_Inequality" to " != "
+                AnalyzeExpression(recurData, expr.Arguments[0]);
+                _result.AppendString(sqlOperatorName);
+                AnalyzeExpression(recurData, expr.Arguments[1]);
+                return;
+            }
+
+            if (expr.Method.DeclaringType == typeof(string))
+            {
+                AnalyzeMethodCall_String(recurData, expr);
+                return;
+            }
+
+            if (expr.Method.DeclaringType == typeof(System.Linq.Queryable) || expr.Method.DeclaringType == typeof(System.Linq.Enumerable))
+            {
+                AnalyzeMethodCall_Queryable(recurData, expr);
+                return;
+            }
+
+
+            //special handling
+            switch (expr.Method.Name)
+            {
+                case "Concat":
+                    {
+                        //this was discontinued after Linq 20006 preview
+                        throw new ApplicationException("L581 Discontinued operand: Concat");
+                    }
+                case "FromOADate":
+                    {
+                        //convert double to DateTime
+                        _result.AppendString("CAST(");
+                        AnalyzeExpression(recurData, expr.Arguments[0]); //it's a static fct - don't pass expr.Object
+                        _result.AppendString(" as smalldatetime)");
+                        return;
+                    }
+
+                default:
+                    //detailed error will be thrown below
+                    break;
+            }
+
+            //check if the function is a stored proc:
+            FunctionAttribute functionAttrib = AttribHelper.GetFunctionAttribute(expr.Method);
+            if (functionAttrib != null)
+            {
+                //it's a stored proc in the database
+                _result.AppendString(functionAttrib.Name + "(");
+                string comma = "";
+                foreach (Expression functionArg in expr.Arguments)
+                {
+                    _result.AppendString(comma); comma = ",";
+                    AnalyzeExpression(recurData, functionArg);
+                }
+                _result.AppendString(")");
+                return;
+            }
+
+            //TODO: throw for any other method - database probably cannot handle such call
+            string msg2 = "L274: Unprepared to map method " + methodName + " (" + expr + ") to SQL";
+            Console.WriteLine(msg2);
+            throw new ApplicationException(msg2);
+            //_result.AppendString(expr.Method.Name);
+        }
+
+        /// <summary>
+        /// handle string.Length, string.StartsWith etc
+        /// </summary>
+        internal void AnalyzeMethodCall_Queryable(RecurData recurData, MethodCallExpression expr)
+        {
+            if (expr.Method.DeclaringType == typeof(System.Linq.Queryable) || expr.Method.DeclaringType == typeof(System.Linq.Enumerable))
+            {
+            }
+            else
+            {
+                throw new ArgumentException("Only Enumerable or Queryable methods allowed, not: " + expr.Method);
+            }
+
+
+            switch (expr.Method.Name)
+            {
+                case "Contains":
+                    {
+                        #region Contains()
+                        AnalyzeExpression(recurData, expr.Arguments[1]); //p.ProductID
+                        _result.AppendString(" IN ( ");
+                        //AnalyzeExpression(recurData, expr.Arguments[0]); //{value(<>c__DisplayClass2).ids}
+                        System.Collections.IEnumerable valueArray = null;
+                        if (expr.Arguments[0].NodeType == ExpressionType.MemberAccess)
+                        {
+                            MemberExpression array1 = expr.Arguments[0].XMember();
+                            ConstantExpression const1 = array1.Expression.XConstant();
+                            object valueStruct = const1.Value;
+                            System.Reflection.FieldInfo memberInfo = array1.Member as System.Reflection.FieldInfo;
+                            object valueObj = memberInfo.GetValue(valueStruct);
+                            //TODO instead of casting to array, process directly as IEnumerable
+                            valueArray = (System.Collections.IEnumerable)valueObj;
+                        }
+                        else if (expr.Arguments[0].NodeType == ExpressionType.NewArrayInit)
+                        {
+                            //handle 'where new string[] { "ALFKI", "WARTH" }.Contains(xxx)'
+                            NewArrayExpression newArrayExpr = expr.Arguments[0] as NewArrayExpression;
+                            List<object> valueList = new List<object>();
+                            foreach (Expression part in newArrayExpr.Expressions)
+                            {
+                                ConstantExpression const1 = part.XConstant();
+                                object valueStruct = const1.Value;
+                                valueList.Add(valueStruct);
+                            }
+                            valueArray = valueList;
+                        }
+
+                        string separator = "";
+                        foreach (object obj in valueArray)
+                        {
+                            string objString = (obj == null || obj is string)
+                                ? (obj as string).QuoteString_Safe()
+                                : obj.ToString();
+
+                            _result.AppendString(separator + objString);
+                            separator = ",";
+                        }
+                        _result.AppendString(" ) ");
+                        return;
+                        #endregion
+                    }
+
+                case "Sum":
+                    {
+                        //extract 'OrderID' from '{g.Sum(o => Convert(o.OrderID))}'
+                        Expression sumExpr1 = expr.Arguments[1].XLambda().Body;
+                        MemberExpression sumExpr2 = null;
+                        switch (sumExpr1.NodeType)
+                        {
+                            //case ExpressionType.Cast: //Cast disappeared in Beta2?!
+                            //    sumExpr2 = sumExpr1.XCastOperand().XMember(); break;
+                            case ExpressionType.MemberAccess:
+                                sumExpr2 = sumExpr1.XMember(); break;
+                            case ExpressionType.Convert:
+                                //eg. {g.Sum(o => Convert(o.OrderID))} (from G08_OrderSumByCustomerID)
+                                sumExpr2 = sumExpr1.XUnary().Operand.XMember();
+                                break;
+                            default:
+                                throw new ArgumentException("L277 Sum(lambda): unprepared for lambda expr " + sumExpr1.NodeType);
+                        }
+                        _result.AppendString("SUM(" + sumExpr2.Member.Name + ")");
+                        return;
+                    }
+                case "Count":
+                    {
+                        //given expr='{g.Count()}', produce Count expression
+                        _result.AppendString("COUNT(*)");
+                        return;
+                    }
+                case "Concat":
+                    {
+                        //this was discontinued after Linq 20006 preview
+                        throw new ApplicationException("L581 Discontinued operand: Concat");
+                    }
+                default:
+                    //detailed error will be thrown below
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// handle string.Length, string.StartsWith etc
+        /// </summary>
+        internal void AnalyzeMethodCall_String(RecurData recurData, MethodCallExpression expr)
+        {
+            if (expr.Method.DeclaringType != typeof(string))
+                throw new ArgumentException("Only string.X method allowed, not: " + expr.Method);
+
+            switch (expr.Method.Name)
+            {
+                case "StartsWith":
+                case "EndWith":
+                case "Contains":
+                    {
+                        //turn "e.Name.StartsWith("X")" -> "e.Name LIKE 'X%'
+                        //turn "e.Name.Contains("X")" -> "e.Name LIKE '%X%'
+                        AnalyzeExpression(recurData, expr.Object);
+                        _result.AppendString(" LIKE ");
+                        AnalyzeExpression(recurData, expr.Arguments[0]);
+                        string paramName = _parent.lastParamName;
+                        string lastParam = _parent.paramMap[paramName] as string;
+                        if (lastParam != null)
+                        {
+                            //modify parameter from X to X%
+                            string modParam = "";
+                            switch (expr.Method.Name)
+                            {
+                                case "StartsWith": modParam = lastParam + "%"; break;
+                                case "EndWith": modParam = "%" + lastParam; break;
+                                case "Contains": modParam = "%" + lastParam + "%"; break;
+                            }
+                            _parent.paramMap[paramName] = modParam;
+                        }
+                    }
+                    return;
+                case "Concat":
+                    {
+                        //this was discontinued after Linq 20006 preview
+                        throw new ApplicationException("L581 Discontinued operand: Concat");
+                    }
+                case "ToLower":
+                case "ToUpper":
+                    {
+                        string sqlFctName = expr.Method.Name == "ToLower" ? "LOWER(" : "UPPER(";
+                        _result.AppendString(sqlFctName);
+                        AnalyzeExpression(recurData, expr.Object);
+                        _result.AppendString(")");
+                        return;
+                    }
+
+                default:
+                    //detailed error will be thrown below
+                    break;
+            }
+
+        }
+
+    }
+}
