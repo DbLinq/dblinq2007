5c281433157b0d20b03c7973e4f3b8799df0550b
picrap <picrap@33146b6e-df29-0410-a1ca-39be37fb9416>
2008-04-09 19:44:32 +0000

    Repository cleanup (3)

--- trunk/DbMetal/bin/run_ingMetal.bat
+++
@@ -1,3 +0,0 @@
-DbMetal.exe -user:LinqUser -database:Northwind -server:localhost -password:linq2 -namespace:nwind -Provider=Ingres /entityInterfaces=IModified /pluralize /sprocs
-
-
--- trunk/DbMetal/bin/run_pgMetal.bat
+++
@@ -1,3 +0,0 @@
-PgsqlMetal.exe -user:LinqUser -database:Northwind -server:localhost -password:linq2 -namespace:nwind -renamesFile:pgsqlFieldRenames.xml -sprocs
-
-
