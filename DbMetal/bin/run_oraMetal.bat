5ca5ede7ae0c3061c25b1a8ac2a6e9357b98c556
Pascal Craponne <pascal@craponne.fr>
2008-04-09 19:44:32 +0000

    Repository cleanup (3)

--- DbMetal/bin/run_oraMetal.bat
+++
@@ -1,5 +0,0 @@
-REM ##
-REM OracleMetal.exe -user:LinqUser -server:localhost -password:linq2 -namespace:nwind -renamesFile:oraFieldRenames.xml
-
-OracleMetal.exe -user:Northwind -database:Northwind -server:localhost -password:linq2 -namespace:nwind -renamesFile:oraFieldRenames.xml
-
