aec7ef0e404621dcc4a15e006a80c982503cef20
Jiri Moudry <gmoudry@gmail.com>
2008-04-14 20:50:00 +0000

    Fix for bug reported by Andrus, where protected fields are being read out multiple times. Test case Z1_AttribHelper_ShouldNotReturnDuplicateFields

--- Tests/Test_NUnit/ReadTest_Operands.cs
+++ Tests/Test_NUnit/ReadTest_Operands.cs
@@ -183,6 +183,19 @@ namespace Test_NUnit_MySql
             Assert.IsTrue(empID == 1);
         }
 
+        //there was a bug where intField1 would be listed multiple times in a select statement:
+        public class Class1 { protected int intField1; }
+        public class Class2 : Class1 { protected int intField2; }
+
+        [Test]
+        public void Z1_AttribHelper_ShouldNotReturnDuplicateFields()
+        {
+            //Andrus pointed out that one of the internal classes that help with reflection
+            //returns fields in duplicate, which kills SQL SELECT and UPDATEs.
+            System.Reflection.MemberInfo[] members = DbLinq.Util.AttribHelper.GetMemberFields(typeof(Class2));
+            Assert.IsTrue(members.Length == 2);
+        }
+
 
     }
 }
