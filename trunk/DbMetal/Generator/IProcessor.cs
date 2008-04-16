92dd57c335febf6cdc1e70c041ed9be7e1296112
picrap <picrap@33146b6e-df29-0410-a1ca-39be37fb9416>
2008-04-16 09:22:33 +0000

    Update to the MIT license Update to CodeDomGenerator, to make it ICodeGenerator compliant (and then parameter compliant)

--- trunk/DbMetal/Generator/IProcessor.cs
+++ trunk/DbMetal/Generator/IProcessor.cs
@@ -1,29 +1,28 @@
 ﻿#region MIT license
-////////////////////////////////////////////////////////////////////
-// MIT license:
-// Permission is hereby granted, free of charge, to any person obtaining
-// a copy of this software and associated documentation files (the
-// "Software"), to deal in the Software without restriction, including
-// without limitation the rights to use, copy, modify, merge, publish,
-// distribute, sublicense, and/or sell copies of the Software, and to
-// permit persons to whom the Software is furnished to do so, subject to
-// the following conditions:
-//
-// The above copyright notice and this permission notice shall be
-// included in all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-//
-// Authors:
-//        Jiri George Moudry
-////////////////////////////////////////////////////////////////////
+// 
+// Copyright (c) 2007-2008 Jiri Moudry
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+// 
 #endregion
 
+using System.Collections.Generic;
 using DbLinq.Logging;
 using DbLinq.Schema.Dbml;
 using DbLinq.Vendor;
@@ -33,10 +32,50 @@ namespace DbMetal.Generator
     public interface IProcessor
     {
         ILogger Logger { get; set; }
+
+        /// <summary>
+        /// The SchemaLoadFactory is used to create the ISchemaLoader (who loads the schema from the database)
+        /// Automatically created by IProcessor implementation
+        /// </summary>
         ISchemaLoaderFactory SchemaLoaderFactory { get; set; }
+
+        /// <summary>
+        /// Whole process, given application parameters
+        /// </summary>
+        /// <param name="args"></param>
         void Process(string[] args);
-        void GenerateCSharp(Parameters parameters, Database dbSchema, ISchemaLoader schemaLoader, string filename);
+        /// <summary>
+        /// Generates a code file, given parameters
+        /// </summary>
+        /// <param name="parameters">Application parameters</param>
+        /// <param name="dbSchema">Loaded schema (from file or database)</param>
+        /// <param name="schemaLoader">Schema loader related to dbSchema</param>
+        /// <param name="filename">Output filename</param>
+        void Generate(Parameters parameters, Database dbSchema, ISchemaLoader schemaLoader, string filename);
+        /// <summary>
+        /// Loads a schema from a database
+        /// </summary>
+        /// <param name="parameters"></param>
+        /// <param name="schemaLoader"></param>
+        /// <returns></returns>
         Database LoadSchema(Parameters parameters, ISchemaLoader schemaLoader);
+        /// <summary>
+        /// Loads a schema from a given file
+        /// </summary>
+        /// <param name="filename"></param>
+        /// <returns></returns>
 		Database LoadSchema(string filename);
+        /// <summary>
+        /// Lists all code generators available in DbMetal
+        /// </summary>
+        /// <returns></returns>
+        IEnumerable<ICodeGenerator> EnumerateCodeGenerators();
+        /// <summary>
+        /// Find a code generator given the /language or filename extension
+        /// </summary>
+        /// <param name="parameters"></param>
+        /// <param name="filename"></param>
+        /// <returns></returns>
+        ICodeGenerator FindCodeGenerator(Parameters parameters, string filename);
     }
 }
