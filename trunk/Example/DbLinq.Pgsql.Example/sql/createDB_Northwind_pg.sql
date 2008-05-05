5502dc42c01e746a2a4b3177b860a0f34d0f948e
picrap <picrap@33146b6e-df29-0410-a1ca-39be37fb9416>
2008-05-05 09:35:20 +0000

    Renamed PostgreSQL "northwind" to "Northwind"

--- trunk/Example/DbLinq.Pgsql.Example/sql/createDB_Northwind_pg.sql
+++ trunk/Example/DbLinq.Pgsql.Example/sql/createDB_Northwind_pg.sql
@@ -2,10 +2,10 @@
 --script to create PostgreSql version of the Northwind test DB
 --####################################################################
 
-DROP DATABASE IF EXISTS northwind;
-CREATE DATABASE northwind WITH OWNER = "LinqUser";
+DROP DATABASE IF EXISTS Northwind;
+CREATE DATABASE Northwind WITH OWNER = "LinqUser";
 
-\connect northwind; 
+\connect Northwind; 
 --Carramba! http://www.postgresqlforums.com/forums/viewtopic.php?f=33&t=10
 --Re: switch database connection in jdbc Postby wagnerch on Tue Jan 02, 2007 3:05 pm 
 --You would need to disconnect and reconnect to the other database, or hold two connections to each database. 
@@ -64,7 +64,7 @@ CREATE TABLE "Products" (
   "UnitsInStock" SMALLINT NULL,
   "UnitsOnOrder" SMALLINT NULL,
   "ReorderLevel" SMALLINT NULL,
-  "Discontinued" BIT NOT NULL,
+  "Discontinued" BOOL NOT NULL,
   PRIMARY KEY("ProductID"),
   CONSTRAINT "FK_prod_catg" FOREIGN KEY ("CategoryID") REFERENCES "Categories"("CategoryID"),
   CONSTRAINT "FK_prod_supp" FOREIGN KEY ("SupplierID") REFERENCES "Suppliers"("SupplierID")
