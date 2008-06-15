f5b49f0a80a2e59de76aff301e4237e4c7a6eb9c
Pascal Craponne <pascal@craponne.fr>
2008-06-15 15:46:51 +0000

    Changes to OracleSqlProvider

--- DbLinq.Oracle/OracleSqlProvider.cs
+++ DbLinq.Oracle/OracleSqlProvider.cs
@@ -28,5 +28,47 @@ namespace DbLinq.Oracle
 {
     public class OracleSqlProvider : SqlProvider
     {
+        protected override string GetLiteralCount(string a)
+        {
+            return "COUNT(*)";
+        }
+
+        protected override string GetLiteralStringLength(string a)
+        {
+            return string.Format("LENGTH({0})", a);
+        }
+
+        protected override string GetLiteralStringToLower(string a)
+        {
+            return string.Format("LOWER({0})", a);
+        }
+
+        protected override string GetLiteralStringToUpper(string a)
+        {
+            return string.Format("UPPER({0})", a);
+        }
+
+        //          SELECT * FROM (
+        //          SELECT a.*, rownum RN FROM (
+        //          select c$.CustomerID, c$.CompanyName from Customers c$ order by c$.CUSTOMERID
+        //          ) a  WHERE rownum <=5
+        //          ) WHERE rn >2
+
+        protected const string LimitedTableName = "LimitedTable___";
+        protected const string LimitedRownum = "Limit___";
+
+        public override string GetLiteralLimit(string select, string limit)
+        {
+            return string.Format(
+                @"SELECT {2}.*, rownum {3} FROM ({4}{0}{4}) {2} WHERE rownum <= {1}",
+                select, limit, LimitedTableName, LimitedRownum, NewLine);
+        }
+
+        public override string GetLiteralLimit(string select, string limit, string offset, string offsetAndLimit)
+        {
+            return string.Format(
+                @"SELECT * FROM ({3}{0}{3}) WHERE {2} > {1}",
+                GetLiteralLimit(select, offsetAndLimit), offset, LimitedRownum, NewLine);
+        }
     }
 }
