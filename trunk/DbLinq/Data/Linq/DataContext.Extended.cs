f196b72b42f96442f5d2075941f6f34f1cbd195f
picrap <picrap@33146b6e-df29-0410-a1ca-39be37fb9416>
2008-06-28 21:27:01 +0000

    Started to split DataContext

---
+++ trunk/DbLinq/Data/Linq/DataContext.Extended.cs
@@ -0,0 +1,63 @@
+﻿#region MIT license
+// 
+// MIT license
+//
+// Copyright (c) 2007-2008 Jiri Moudry, Pascal Craponne
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+// 
+#endregion
+
+using System;
+using System.Diagnostics;
+
+#if MONO_STRICT
+namespace System.Data.Linq
+#else
+namespace DbLinq.Data.Linq
+#endif
+{
+    partial class DataContext
+    {
+        /// <summary>
+        /// Pings database
+        /// </summary>
+        /// <returns></returns>
+        public bool DatabaseExists()
+        {
+            try
+            {
+                using (DatabaseContext.OpenConnection())
+                {
+                    //command: "SELECT 11" (Oracle: "SELECT 11 FROM DUAL")
+                    string SQL = Vendor.SqlPingCommand;
+                    int result = Vendor.ExecuteCommand(this, SQL);
+                    return result == 11;
+                }
+            }
+            catch (Exception ex)
+            {
+                if (true)
+                    Trace.WriteLine("DatabaseExists failed:" + ex);
+                return false;
+            }
+        }
+
+    }
+}
