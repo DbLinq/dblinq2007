3af898d47557067de13f6d18eaa944eb7490ec96
pibgeus <pibgeus@33146b6e-df29-0410-a1ca-39be37fb9416>
2008-10-13 18:39:23 +0000

    r911 broken, one file wasn't uploaded

---
+++ trunk/tests/Test_NUnit/DataContext.cs
@@ -0,0 +1,80 @@
+﻿#region MIT license
+// 
+// MIT license
+//
+// Copyright (c) 2007-2008 Jiri Moudry, Pascal Craponne, Pascal Craponne, Pascal Craponne, Pascal Craponne, Pascal Craponne, Pascal Craponne, Pascal Craponne, Pascal Craponne
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+// 
+#endregion
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Test_NUnit;
+using System.Data.Linq;
+
+#if !MONO_STRICT
+using nwind;
+#else
+using MsNorthwind;
+#endif
+
+#if MYSQL
+    namespace Test_NUnit_MySql
+#elif ORACLE
+#if ODP
+        namespace Test_NUnit_OracleODP
+#else
+        namespace Test_NUnit_Oracle
+#endif
+#elif POSTGRES
+    namespace Test_NUnit_PostgreSql
+#elif SQLITE
+    namespace Test_NUnit_Sqlite
+#elif INGRES
+    namespace Test_NUnit_Ingres
+#elif MSSQL
+#if MONO_STRICT
+namespace Test_NUnit_MsSql_Strict
+#else
+namespace Test_NUnit_MsSql
+#endif
+#else
+#error unknown target
+#endif
+{
+    [TestFixture]
+    public class DataContext : TestBase
+    {
+        [Test]
+        public void GetCommand()
+        {
+            var db = CreateDB();
+            string country = "Spain";
+            var cust = db.Customers.Where(c => c.Country == country);
+            var dbCommand=db.GetCommand(cust);
+            
+
+            Assert.IsNotNull(dbCommand.CommandText);
+            Assert.Greater(dbCommand.Parameters.Count, 0);
+        }
+    }
+}
