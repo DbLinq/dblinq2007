c559651fe160b119bfe4ca0b83273cb90ef9208e
jcarrascal <jcarrascal@33146b6e-df29-0410-a1ca-39be37fb9416>
2008-10-16 02:24:17 +0000

    Removed files generated by Visual Studio during the build process. Passes the insert example but it's not retrieving the generated value yet.

--- examples/DbLinq.FbSql.Example/Program.cs
+++ examples/DbLinq.FbSql.Example/Program.cs
@@ -77,18 +77,18 @@ namespace DbLinq.FbSql.Example
             Console.WriteLine("Press enter to continue.");
             Console.ReadKey();
 
-            //// BUG: auto_increment columns aren't supported on Firebird SQL yet.
-            //Console.Clear();
-            //logger.Write(Level.Debug, "db.Orders.Add(new Order { ProductID = 7, CustomerId = 1, OrderDate = DateTime.Now });");
-            //db.Orders.InsertOnSubmit(new Order { EmployeeID = 1, CustomerID = "ALFKI", OrderDate = DateTime.Now });
-            //db.SubmitChanges();
-            //logger.Write(Level.Debug, "db.Orders.Add(new Order { ProductID = 2, CustomerId = 2, OrderDate = DateTime.Now });");
-            //db.Orders.InsertOnSubmit(new Order { EmployeeID = 1, CustomerID = "ALFKI", OrderDate = DateTime.Now });
-            //db.SubmitChanges();
-            //foreach (var v in db.Orders)
-            //    ObjectDumper.Write(v);
-            //logger.Write(Level.Information, "Press enter to continue.");
-            //Console.ReadKey();
+            // BUG: auto_increment columns aren't supported on Firebird SQL yet.
+            Console.Clear();
+            Console.WriteLine("db.Orders.Add(new Order { ProductID = 7, CustomerId = 1, OrderDate = DateTime.Now });");
+            db.Orders.InsertOnSubmit(new Order { EmployeeID = 1, CustomerID = "ALFKI", OrderDate = DateTime.Now });
+            db.SubmitChanges();
+            Console.WriteLine("db.Orders.Add(new Order { ProductID = 2, CustomerId = 2, OrderDate = DateTime.Now });");
+            db.Orders.InsertOnSubmit(new Order { EmployeeID = 1, CustomerID = "ALFKI", OrderDate = DateTime.Now });
+            db.SubmitChanges();
+            foreach (var v in db.Orders)
+                ObjectDumper.Write(v);
+            Console.WriteLine("Press enter to continue.");
+            Console.ReadKey();
 
             Console.Clear();
             Console.WriteLine("db.Orders.Remove(db.Orders.First());");
