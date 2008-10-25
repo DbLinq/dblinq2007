a015a1fe23f950d066a7d3e02897eeb87f02b2ae
dev.null.nix <dev.null.nix@33146b6e-df29-0410-a1ca-39be37fb9416>
2008-10-25 18:47:22 +0000

    Rewrite of AttributedMetaModel to support non strong typed DataContext usage. Corrected association mapping code. The table mapping of DataContext now uses the type as key and not the type name.

--- src/DbLinq/Data/Linq/DataContext.cs
+++ src/DbLinq/Data/Linq/DataContext.cs
@@ -58,6 +58,7 @@ using DbLinq.Vendor;
 using DbLinq.Data.Linq.Database;
 using DbLinq.Data.Linq.Database.Implementation;
 using System.Linq.Expressions;
+using System.Reflection.Emit;
 
 #if MONO_STRICT
 namespace System.Data.Linq
@@ -67,7 +68,8 @@ namespace DbLinq.Data.Linq
 {
     public partial class DataContext : IDisposable
     {
-        private readonly Dictionary<string, ITable> _tableMap = new Dictionary<string, ITable>();
+        //private readonly Dictionary<string, ITable> _tableMap = new Dictionary<string, ITable>();
+		private readonly Dictionary<Type, ITable> _tableMap = new Dictionary<Type, ITable>();
 
         public MetaModel Mapping { get; private set; }
         // PC question: at ctor, we get a IDbConnection and the Connection property exposes a DbConnection
@@ -247,20 +249,45 @@ namespace DbLinq.Data.Linq
             Mapping = mappingSource.GetModel(GetType());
         }
 
+		/// <summary>
+		/// Checks if the table is allready mapped or maps it if not.
+		/// </summary>
+		/// <param name="tableType">Type of the table.</param>
+		/// <exception cref="InvalidOperationException">Thrown if the table is not mappable.</exception>
+		private void CheckTableMapping(Type tableType)
+		{
+			//This will throw an exception if the table is not found
+			if(Mapping.GetTable(tableType) == null)
+			{
+				throw new InvalidOperationException("The type '" + tableType.Name + "' is not mapped as a Table.");
+			}
+		}
 
-        public Table<TEntity> GetTable<TEntity>() where TEntity : class
+		/// <summary>
+		/// Returns a Table for the type TEntity.
+		/// </summary>
+		/// <exception cref="InvalidOperationException">If the type TEntity is not mappable as a Table.</exception>
+		/// <typeparam name="TEntity">The table type.</typeparam>
+    	public Table<TEntity> GetTable<TEntity>() where TEntity : class
         {
             return (Table<TEntity>)GetTable(typeof(TEntity));
         }
 
+		/// <summary>
+		/// Returns a Table for the given type.
+		/// </summary>
+		/// <param name="type">The table type.</param>
+		/// <exception cref="InvalidOperationException">If the type is not mappable as a Table.</exception>
         public ITable GetTable(Type type)
         {
             lock (_tableMap)
             {
-                string tableName = type.FullName;
                 ITable tableExisting;
-                if (_tableMap.TryGetValue(tableName, out tableExisting))
+				if (_tableMap.TryGetValue(type, out tableExisting))
                     return tableExisting;
+
+				//Check for table mapping
+				CheckTableMapping(type);
 
                 var tableNew = Activator.CreateInstance(
                                   typeof(Table<>).MakeGenericType(type)
@@ -269,7 +296,7 @@ namespace DbLinq.Data.Linq
                                   , new object[] { this }
                                   , System.Globalization.CultureInfo.CurrentCulture) as ITable;
 
-                _tableMap[tableName] = tableNew;
+                _tableMap[type] = tableNew;
                 return tableNew;
             }
         }
@@ -683,7 +710,7 @@ namespace DbLinq.Data.Linq
         /// </summary>
         public IEnumerable<TResult> ExecuteQuery<TResult>(string query, params object[] parameters) where TResult : class, new()
         {
-            GetTable<TResult>();
+            //GetTable<TResult>();
             foreach (TResult result in ExecuteQuery(typeof(TResult), query, parameters))
                 yield return result;
         }
