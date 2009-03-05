430772c348f83fe9f0b5e79e562f5b96bb515836
Jonathan Pryor <jonp@xamarin.com>
2009-03-05 04:58:33 +0000

    Remove the horrible #if-fest within src/DbLinq/Test/Providers/TestBase.cs by using partial classes, so that each test project can customize TestBase behavior to create the appropriate Command, Connection, and Vendor types, etc.

--- src/DbLinq/Test/Providers/TestBase.cs
+++ src/DbLinq/Test/Providers/TestBase.cs
@@ -25,6 +25,7 @@
 #endregion
 
 using System;
+using System.Data;
 using System.IO;
 using System.Xml;
 using System.Reflection;
@@ -38,57 +39,14 @@ using MsNorthwind;
 using System.Data.Linq;
 #endif
 
-#if ORACLE
-#if ODP
-using xint = System.Int32;
-using XSqlConnection = Oracle.DataAccess.Client.OracleConnection;
-using XSqlCommand = Oracle.DataAccess.Client.OracleCommand;
-#else
-using xint = System.Int32;
-using XSqlConnection = System.Data.OracleClient.OracleConnection;
-using XSqlCommand = System.Data.OracleClient.OracleCommand;
-#endif
-#elif POSTGRES
-using xint = System.Int32;
-using XSqlConnection = Npgsql.NpgsqlConnection;
-using XSqlCommand = Npgsql.NpgsqlCommand;
-#elif SQLITE
-using System.Data.SQLite;
-using XSqlConnection = System.Data.SQLite.SQLiteConnection;
-using XSqlCommand = System.Data.SQLite.SQLiteCommand;
-#elif MSSQL
-using XSqlConnection = System.Data.SqlClient.SqlConnection;
-using XSqlCommand = System.Data.SqlClient.SqlCommand;
-using xint = System.UInt32;
-
-#elif INGRES
-using XSqlConnection = Ingres.Client.IngresConnection;
-using XSqlCommand = Ingres.Client.IngresCommand;
-using xint = System.UInt32;
-#elif FIREBIRD
-using XSqlConnection = FirebirdSql.Data.FirebirdClient.FbConnection;
-using XSqlCommand = FirebirdSql.Data.FirebirdClient.FbCommand;
-using xint = System.UInt32;
-#else
-using XSqlConnection = MySql.Data.MySqlClient.MySqlConnection;
-using XSqlCommand = MySql.Data.MySqlClient.MySqlCommand;
-using xint = System.UInt32;
-
-
-#endif
-
 namespace Test_NUnit
 {
     /// <summary>
     /// base class for ReadTest and WriteTest. 
     /// Provides CreateDB(), Conn, and stringComparisonType.
     /// </summary>
-    public abstract class TestBase
+    public abstract partial class TestBase
     {
-#if SQLITE
-        static bool doRecreate = true;
-#endif
-
         public string DbServer
         {
             get
@@ -112,39 +70,22 @@ namespace Test_NUnit
                 return stringConnection;
             }
         }
-        XSqlConnection _conn;
-        public XSqlConnection Conn
+        IDbConnection _conn;
+        public IDbConnection Conn
         {
             get
             {
-                if (_conn == null) { _conn = new XSqlConnection(connStr); _conn.Open(); }
+                if (_conn == null) { _conn = CreateConnection(connStr); _conn.Open(); }
                 return _conn;
             }
         }
-
-#if POSTGRES || ORACLE || INGRES
-        //Postgres sorting: A,B,C,X,d
-        public const StringComparison stringComparisonType = StringComparison.Ordinal; 
-#else
-        //Mysql,Oracle sorting: A,B,C,d,X
-        public const StringComparison stringComparisonType = StringComparison.InvariantCulture;
-#endif
 
         //public Northwind CreateDB()
         //{
         //    return CreateDB(System.Data.ConnectionState.Closed);
         //}
 
-        private static void CheckRecreateSqlite()
-        {
-#if SQLITE
-            if (doRecreate)
-            {
-                File.Copy(@"..\src\Northwind.db3", "Northwind.db3", true);
-                doRecreate = false;
-            }
-#endif
-        }
+        static partial void CheckRecreateSqlite ();
 
         public Northwind CreateDB()
         {
@@ -154,45 +95,19 @@ namespace Test_NUnit
         public Northwind CreateDB(System.Data.ConnectionState state)
         {
             CheckRecreateSqlite();
-            var conn = new XSqlConnection(connStr);
+            var conn = CreateConnection(connStr);
             if (state == System.Data.ConnectionState.Open)
                 conn.Open();
             var db = new Northwind(conn) { Log = Console.Out };
             return db;
         }
 
-#if !MONO_STRICT
-        public DbLinq.Vendor.IVendor CreateVendor()
-        {
-            var vendor =
-#if MYSQL
-    new DbLinq.MySql.MySqlVendor()
-#elif ORACLE
-    new DbLinq.Oracle.OracleVendor()
-#elif POSTGRES
-    new DbLinq.PostgreSql.PgsqlVendor()
-#elif SQLITE
-    new DbLinq.Sqlite.SqliteVendor()
-#elif INGRES
-    new DbLinq.Ingres.IngresVendor()
-#elif MSSQL
-    new DbLinq.SqlServer.SqlServerVendor()
-#elif FIREBIRD
-    new DbLinq.Firebird.FirebirdVendor()
-#else
-    #error unknown target
-#endif
-            ;
-            return vendor;
-        }
-#endif
-
         /// <summary>
         /// execute a sql statement, return an Int64.
         /// </summary>
         public long ExecuteScalar(string sql)
         {
-            using (var cmd = new XSqlCommand(sql, Conn))
+            using (var cmd = CreateCommand(sql, Conn))
             {
                 object oResult = cmd.ExecuteScalar();
                 Assert.IsNotNull("Expecting result, instead got null. (sql=" + sql + ")");
@@ -206,7 +121,7 @@ namespace Test_NUnit
         /// </summary>
         public void ExecuteNonQuery(string sql)
         {
-            using (var cmd = new XSqlCommand(sql, Conn))
+            using (var cmd = CreateCommand(sql, Conn))
             {
                 int iResult = cmd.ExecuteNonQuery();
             }
