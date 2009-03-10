864f8e113724a34fc6c79071ae15b5a416f42887
jonmpryor <jonmpryor@33146b6e-df29-0410-a1ca-39be37fb9416>
2009-03-10 18:56:36 +0000

    Merge the testing-no-db branch with trunk.
    Highlights:
      - *Start* creating Vendor tests that don't require a DB to execute.
        These are in the *_ndb* projects.
      - Split up the test projects into separate directories, e.g.
        tests/Test_NUnit/Test_NUnit_Sqlite.csproj is now
        src/DbLinq.Sqlite/Test/DbLinq.Sqlite_test.csproj.
      - Removal of some bits of #if "cruft" from the unit tests to simplify the
        MONO_STRICT semantics.
      - Mono.Data.Sqlite support.
      - Place NUnit test assemblies into the `build' directory, along with the
        rest of the DbLinq* assemblies.
      - Create a top-level build.dbg directory for debug assemblies

--- trunk/examples/DbLinq.Mssql.Example/nwind/originalMSNorthwind.designer.cs
+++ trunk/examples/DbLinq.Mssql.Example/nwind/originalMSNorthwind.designer.cs
@@ -9,7 +9,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace MsNorthwind
+namespace nwind
 {
 	using System.Data.Linq;
 	using System.Data.Linq.Mapping;
