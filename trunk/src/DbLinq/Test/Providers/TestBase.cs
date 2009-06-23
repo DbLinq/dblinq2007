0d53cd8c50c599278c819d4d2de4796bd99187c0
jonmpryor <jonmpryor@33146b6e-df29-0410-a1ca-39be37fb9416>
2009-06-23 20:29:23 +0000

    Fix a build break.
    The problems are numerous:
    
      - DbLinq.SqlServer_test_strict.csproj wanted to include NorthwindCustom.cs,
        which didn't exist (it wasn't properly linked).
      - Properly linking NorthwindCustom.cs prevented it from building properly,
        due to the NorthwindCustom(IDbConnection,IVendor) constructor.
    
    Solution: Remove the NorthwindCustom type, and instead merge the contents into
    a partial Northwind instance.  This allows us to ignore constructor overloads
    (as Northwind will handle them).  Rename the CategoryCustom type to
    NoStorageCategory, and rename the NorthwindCustom.Categories property to
    Northwind.NoStorageCategories.
    
    Fix the unit tests to deal with these changes.
    
    Finally, remove originalMSNorthwind.* from the DbLinq.Mssql.Example build (why
    are we using L2SQL output here anyway?!), because the DbLinq.Mssql.Example
    project was including NorthwindCustom.cs, but we were getting Table<T>
    ambiguity errors as MONO_STRICT wasn't defined, so it was trying to use
    DbLinq's Table<T>, but since originalMSNorthwind.designer.cs was used, we were
    using L2SQL's Table<T> --> BAD.  Just use DbLinq, like we should.
    
    Things should now build.  Test?  Not so much.  At least it builds.

--- trunk/src/DbLinq/Test/Providers/TestBase.cs
+++ trunk/src/DbLinq/Test/Providers/TestBase.cs
@@ -138,24 +138,6 @@ namespace Test_NUnit
             return db;
         }
 
-#if POSTGRES || MSSQL || L2SQL
-
-        public NorthwindCustom CreateCustomDB()
-        {
-            return CreateCustomDB(System.Data.ConnectionState.Closed);
-        }
-
-        public NorthwindCustom CreateCustomDB(System.Data.ConnectionState state)
-        {
-            CheckRecreateSqlite();
-            var conn = CreateConnection(connStr);
-            if (state == System.Data.ConnectionState.Open)
-                conn.Open();
-            var db = new NorthwindCustom(conn) { Log = Console.Out };
-            return db;
-        }     
-#endif
-        
         /// <summary>
         /// execute a sql statement, return an Int64.
         /// </summary>
--- trunk/src/DbLinq/Util/MemberInfoExtensions.cs
+++ trunk/src/DbLinq/Util/MemberInfoExtensions.cs
@@ -54,6 +54,7 @@ namespace DbLinq.Util
                 return null;
             if (memberInfo is Type)
                 return (Type)memberInfo;
+            Console.WriteLine("# Error: Found member: type={0}; name={1}", memberInfo.GetType().FullName, memberInfo.Name);
             throw new ArgumentException();
         }
 
