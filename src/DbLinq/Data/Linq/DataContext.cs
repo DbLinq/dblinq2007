ff744ba9774cce605a2602cc8d37be67044e90c6
admin@yuniti.com <admin@yuniti.com@33146b6e-df29-0410-a1ca-39be37fb9416>
2009-08-14 15:46:45 +0000

    * Implement StorageAccessor and MemberAccessor

--- src/DbLinq/Data/Linq/DataContext.cs
+++ src/DbLinq/Data/Linq/DataContext.cs
@@ -794,18 +794,14 @@ namespace DbLinq.Data.Linq
             }
         }
 
+		private static MethodInfo _WhereMethod = typeof(Queryable).GetMethods().First(m => m.Name == "Where");
         private object GetOtherTableQuery(Expression predicate, ParameterExpression parameter, Type otherTableType, IQueryable otherTable)
         {
             //predicate: other.EmployeeID== "WARTH"
             Expression lambdaPredicate = Expression.Lambda(predicate, parameter);
             //lambdaPredicate: other=>other.EmployeeID== "WARTH"
 
-            var whereMethod = typeof(Queryable)
-                              .GetMethods().First(m => m.Name == "Where")
-                              .MakeGenericMethod(otherTableType);
-
-
-            Expression call = Expression.Call(whereMethod, otherTable.Expression, lambdaPredicate);
+			Expression call = Expression.Call(_WhereMethod.MakeGenericMethod(otherTableType), otherTable.Expression, lambdaPredicate);
             //Table[EmployeesTerritories].Where(other=>other.employeeID="WARTH")
 
             return otherTable.Provider.CreateQuery(call);
