b11938b85d7b7afeeac6658b4905e24d6f23fcfc
Jonathan Pryor <jonp@xamarin.com>
2009-11-20 20:08:46 +0000

    Fix table name generation, column reading; start FK generation.
    When used against SQLite, most of the columns were missing because
    SchemaLoader was GetFullDbName(table, schema), where if schema was null
    then table is returned.  However, DbSchemaLoader.ReadTables() wasn't inserting
    null as the schema, but "" instead.  The result was e.g. '.Employees' from
    GetFullDbName(), which prevent the table from being found within
    SchemaLoader.LoadColumns().
    
    Column reading was also screwy because we weren't setting
    DataTableColumn.FullType, resulting in ColumnAttribute.DbType being empty in
    the generated source.
    
    Finally, found that this is a partially doomed effort: SQL Server doesn't
    appear to make foreign-key information available from the DataTable, so
    currently foreign-key generation is restricted to the SQLite provider (which
    does provide useful information from the DataTable).

--- src/DbLinq/Vendor/DbSchemaLoader.cs
+++ src/DbLinq/Vendor/DbSchemaLoader.cs
@@ -29,7 +29,57 @@ namespace DbLinq.Vendor
 
         protected override void LoadConstraints(Database schema, DbLinq.Schema.SchemaName schemaName, IDbConnection conn, DbLinq.Schema.NameFormat nameFormat, Names names)
         {
-            throw new NotImplementedException();
+            DbConnection c = (DbConnection) conn;
+            var foreignKeys = c.GetSchema("ForeignKeys");
+
+            int iConstName  = foreignKeys.Columns.IndexOf("CONSTRAINT_NAME");
+            int iConstType  = foreignKeys.Columns.IndexOf("CONSTRAINT_TYPE");
+            int iFromColumn = foreignKeys.Columns.IndexOf("FKEY_FROM_COLUMN");
+            int iFromSchema = foreignKeys.Columns.IndexOf("TABLE_SCHEMA");
+            int iFromTable  = foreignKeys.Columns.IndexOf("TABLE_NAME");
+            int iToColumn   = foreignKeys.Columns.IndexOf("FKEY_TO_COLUMN");
+            int iToSchema   = foreignKeys.Columns.IndexOf("FKEY_TO_SCHEMA");
+            int iToTable    = foreignKeys.Columns.IndexOf("FKEY_TO_TABLE");
+
+            if (iConstName < 0 || iConstType < 0 ||
+                    iFromColumn < 0 || iFromSchema < 0 || iFromTable < 0 ||
+                    iToColumn < 0 || iToSchema < 0 || iToTable < 0)
+            {
+                WriteErrorLine("Database connection '{0}' doesn't support querying foreign key information.",
+                    c.GetType().Name);
+                return;
+            }
+
+            foreach (DataRow r in foreignKeys.Rows)
+            {
+                var fromTable = GetValue(r, iFromTable, (string) null);
+                var fromSchema = GetValue(r, iFromSchema, (string) null);
+                var fromColumn = GetValue(r, iFromColumn, (string) null);
+
+                string fullFromTable = GetFullDbName(fromTable, fromSchema);
+                DbLinq.Schema.Dbml.Table table = schema.Tables.FirstOrDefault(t => fullFromTable == t.Name);
+                if (table == null)
+                {
+                    WriteErrorLine("ERROR L46: Table '" + fromTable + "' not found for column " + fromColumn);
+                    continue;
+                }
+
+                var constraintType = GetValue(r, iConstType, (string) null);
+                var toTable = GetValue(r, iToTable, (string) null);
+
+                if (constraintType == "FOREIGN KEY" && toTable != null)
+                {
+                    var constraintName = GetValue(r, iConstName, (string) null);
+                    var toColumn = GetValue(r, iToColumn, (string) null);
+                    var toSchema = GetValue(r, iToSchema, (string) null);
+                    Console.WriteLine("# processing FK: {0}.{1}.{2} -> {3}.{4}.{5}",
+                        fromSchema, fromTable, fromColumn, toSchema, toTable, toColumn);
+                    LoadForeignKey(schema, table, 
+                            fromColumn, fromTable, fromSchema,
+                            toColumn, toTable, toSchema,
+                            constraintName, nameFormat, names);
+                }
+            }
         }
 
         private DataTable GetSchema(DbConnection connection, string schema)
@@ -48,7 +98,7 @@ namespace DbLinq.Vendor
             if (foriegnKeys == null)
                 return;
 #endif
-            throw new NotImplementedException();
+            // throw new NotImplementedException();
         }
 
         protected override void LoadStoredProcedures(Database schema, DbLinq.Schema.SchemaName schemaName, IDbConnection conn, DbLinq.Schema.NameFormat nameFormat)
@@ -58,7 +108,7 @@ namespace DbLinq.Vendor
             if (foriegnKeys == null)
                 return;
 #endif
-            throw new NotImplementedException();
+           //  throw new NotImplementedException();
         }
 
         protected override IList<IDataTableColumn> ReadColumns(IDbConnection connection, string databaseName)
@@ -81,21 +131,45 @@ namespace DbLinq.Vendor
             var columns = new List<IDataTableColumn>();
             foreach (DataRow c in dbColumns.Rows)
             {
+                var sqlType     = c[iSqlType].ToString().Trim();
+                var tableName   = GetValue<string>(c, iTable, null);
+                var tableSchema = GetValue<string>(c, iSchema, null);
+                var columnName  = GetValue<string>(c, iColumn, null);
+
+                if (sqlType.Length == 0)
+                {
+                    // Column has no type; wtf are we supposed to do?
+                    // This happens w/ SQLite while processing Northwind.db3 for the 
+                    // 'Customer and Suppliers by City' view, Relationship column.
+                    Console.Error.WriteLine("Warning: The column '{0}.{1}.{2}' could not be imported because the column's DATA_TYPE is empty.",
+                            tableSchema, tableName, columnName);
+                    continue;
+                }
+
                 var v = new DataTableColumn()
                 {
-                    ColumnName      = (string) c[iColumn],
+                    ColumnName      = columnName,
                     DefaultValue    = c[iDefValue].ToString(),
+                    FullType        = sqlType,
                     Length          = (long) (int) c[iMaxLen],
-                    ManagedType     = typeMap[(string) c[iSqlType]],
+                    ManagedType     = typeMap[sqlType],
                     Nullable        = (bool) c[iNullable],
-                    Precision       = c[iNumPrec] is DBNull ? (int?) null : (int) c[iNumPrec],
-                    SqlType         = (string) c[iSqlType],
-                    TableName       = (string) c[iTable],
-                    TableSchema     = (string) c[iSchema],
+                    Precision       = GetValue<int?>(c, iNumPrec, null),
+                    SqlType         = sqlType,
+                    TableName       = tableName,
+                    TableSchema     = tableSchema,
                 };
                 columns.Add(v);
             }
             return columns;
+        }
+
+        private static T GetValue<T>(DataRow r, int index, T defaultValue)
+        {
+            var v = r[index];
+            if (v is DBNull)
+                return defaultValue;
+            return (T) v;
         }
 
         private Dictionary<string, string> GetSqlToManagedTypeMapping(DbConnection connection)
@@ -116,10 +190,11 @@ namespace DbLinq.Vendor
             List<IDataName> tables = new List<IDataName>();
             foreach (DataRow table in dbTables.Rows)
             {
+                var schema = GetValue(table, iSchema, (string) null);
                 tables.Add(new DataName()
                 {
                     Name    = table[iName].ToString(),
-                    Schema  = table[iSchema].ToString(),
+                    Schema  = string.IsNullOrEmpty(schema) ? null : schema,
                 });
             }
             return tables;
