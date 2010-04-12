ea67232fb5303ba7caf57aadab104940f0e2f945
jonmpryor <jonmpryor@33146b6e-df29-0410-a1ca-39be37fb9416>
2010-04-12 14:23:51 +0000

    Fix Database verification.
    We need to use association.Member, not association.Name, as .Name is
    manipulated for case/etc., while .Member is not.
    
    Fixes Issue 233.

--- trunk/src/DbMetal/Generator/Implementation/Processor.cs
+++ trunk/src/DbMetal/Generator/Implementation/Processor.cs
@@ -139,7 +139,7 @@ namespace DbMetal.Generator.Implementation
             {
                 var otherType           = database.Tables.Single(t => t.Type.Name == association.Type).Type;
                 var otherAssociation    = otherType.Associations.Single(a => a.Type == table.Type.Name && a.ThisKey == association.OtherKey);
-                var otherColumn         = otherType.Columns.Single(c => c.Name == association.OtherKey);
+                var otherColumn         = otherType.Columns.Single(c => c.Member == association.OtherKey);
 
                 if (association.CardinalitySpecified && association.Cardinality == Cardinality.Many && association.IsForeignKey)
                 {
